5d34e73090e11f99d36a3d2acbb00fac3939a74d
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-06-06 20:38:27 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Gallery from './pages/Gallery';
 
-function App() {
-  return <Home />;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
