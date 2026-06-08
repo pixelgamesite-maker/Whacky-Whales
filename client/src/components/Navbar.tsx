5206f032a480f1559c550592a1010563714c3f76
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-06-08 01:02:12 +0100

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
 
   const navLinks = [
     { label: 'Home', href: '/' },
-    { label: 'Gallery', href: '/gallery' },
+    { label: 'Grid Maker', href: '/gallery' },
   ];
 
   const socials = [
