f36e30ab5219f3e4939da29b467262e48ec3d1bc
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-06-09 02:02:50 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Gallery from './pages/Gallery';
+import GifMaker from './pages/MemeGenerator';
+import WhackyPod from './pages/WhackyPod';
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/meme-generator" element={<MemeGenerator />} />
+        <Route path="/whacky-pod" element={<WhackyPod />} />
       </Routes>
     </BrowserRouter>
   );
